904d70c714efe16c0311b5a2617b2c675d07b05e
Robert Scalice <r.scalice@live.com>
2026-03-17 07:27:52 -0400

    Initial commit

---
+++ app/src/MainWindow.cpp
@@ -0,0 +1,15 @@
+#include "MainWindow.h"
+#include "ui_MainWindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowTitle("Boid Flocking Qt App");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
